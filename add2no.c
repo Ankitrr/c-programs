a8093720f729b34b4ff1a98df62cabe21592cd09
Ankitrr <49330924+Ankitrr@users.noreply.github.com>
2019-04-06 12:02:51 +0530

    Create add2no.c

---
+++ add2no.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include<conio.h>
+int input(int*a,int*b)
+{
+    printf("enter a=\n");
+    scanf("%d",&a);
+    printf("enterb=\n");
+    scanf("%d",&b)
+}
+int add 2 no(int a,int b);
+{
+    c=a+b;
+    return c;
+}
+int otput(inta,intb,intc)
+{
+    printf("the Adition of two number",a,b,c);
+
+
+void output(inta,intb,intc)
+{
+    c=a+b;
+    printf("the adition of %d+%d=%d",a,b,c);
+}
+int main()
+int(a,b,c)
+}
+}
